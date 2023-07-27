22490762c0ad67f4659536303fa2d6b80f780923
rajchatterz <iamraj.space@gmail.com>
2023-07-27 23:31:34 +0530

    done with the update and updatemany method in mongodb

--- findOne.js
+++ findOne.js
@@ -1,4 +1,4 @@
-const {MongoClient,ObjectId} = require('mongodb')
+const {MongoClient} = require('mongodb')
 require('dotenv').config()
 const url = process.env.MONGO_URL
 const safeUrl = `${url.slice(0,14)}****${url.slice(30,31)}****${url.slice(47)}`
@@ -15,14 +15,13 @@ const dbconnection = async()=>{
         console.error(`find a error while connecting with the database ${error}`);
     }
 }
-const updateFilter = { _id:  ObjectId("64c2889ce89330965e131d10") }
-
-const documentToUpdate = {balance:{$inc:10000000}}
+const documentToFind = {balance:{$gt:456000}}
 const main = async()=>{
     try {
         await dbconnection()
-        const result = collectionName.updateOne(updateFilter,documentToUpdate)
-        (await result).modifiedCount>0?console.log(`update`):console.log("not update")
+        const result = collectionName.find(documentToFind)
+        const docCount = collectionName.countDocuments(documentToFind)
+        await result.forEach((e)=>{console.log(e)})
         console.log(`Found ${await docCount} documents`)
     } catch (error) {
         console.error(`error finding dicument ${error}`);
