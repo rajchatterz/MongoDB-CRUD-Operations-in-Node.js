6bba3872338b41bd6550cfdff82230e3b8d8c7f1
rajchatterz <iamraj.space@gmail.com>
2023-07-27 20:53:11 +0530

    done with the insertMany method

--- app.js
+++ app.js
@@ -4,29 +4,38 @@ const url = process.env.MONGO_URL
 const client = new MongoClient(url)
 const dbname = "bank"
 const collection_name = "accounts"
-
+const safeURI = `${url.slice(0, 14)}****${url.slice(30, 31)}****${url.slice(47)}`
 const accountCollection = client.db(dbname).collection(collection_name)
 
 const dbconnection = async ()=>{
     try {
         await client.connect()
-        console.log(`connected to the ${dbname} with the url name`)
+        console.log(`connected to the ${dbname} with the url name${safeURI}`)
     } catch (error) {
         console.error('error fetching while connecting to the database',error);
     }
 }
-const sampleAccount = {
-    account_holder:"Raj Chatterjee",
-    account_id:"0000001",
-    account_type:"Saving",
-    balance:456000,
-    last_update:new Date()
-}
+const sampleAccount = [
+    {
+        account_id:"727217272",
+        account_holder:"JOsh malon",
+        account_type:"current",
+        balance:7477474,
+        last_seen:new Date()
+    },
+    {
+        account_id:"y263833",
+        account_holder:"eva melon",
+        account_type:"business",
+        balance:8377388,
+        last_seen:new Date()
+    }
+]
 
 const main = async()=>{
     try {
         await dbconnection()
-        let result = await accountCollection.insertOne(sampleAccount)
+        let result = await accountCollection.insertMany(sampleAccount)
         console.log(`inserted document ${result.account_id}`)
     } catch (error) {
         console.log(`error while inserting the document ${error}`)
